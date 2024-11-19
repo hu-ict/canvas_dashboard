9bfeffa7f8a75d069a3750bf82ac5b2a1601aa6a
Sprinkhaan <84741733+Sprinkhaan@users.noreply.github.com>
2024-11-19 14:36:37 +0100

    env variables

--- keycloak_config.py
+++ keycloak_config.py
@@ -5,6 +5,6 @@ from keycloak.keycloak_openid import KeycloakOpenID
 keycloak_openid = KeycloakOpenID(
     server_url=os.getenv("KEYCLOAK_SERVER_URL"),
     client_id=os.getenv("KEYCLOAK_CLIENT_ID"),
-    realm_name=os.getenv("KEYCLOAK_REALM_NAME"),
-    client_secret_key=os.getenv("KEYCLOAK_CLIENT_SECRET_KEY"),
+    realm_name=os.getenv("KEYCLOAK_REALM"),
+    client_secret_key=os.getenv("KEYCLOAK_CLIENT_SECRET"),
 )
