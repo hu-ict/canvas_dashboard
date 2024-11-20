74fba89d7d81f37e52676ee86c617d5717beee6a
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2024-11-20 11:02:18 +0100

    - teacher kan dashboard zien

--- src/auth.py
+++ src/auth.py
@@ -1,38 +1,56 @@
 # src/auth.py
 from functools import wraps
+
+import jwt
 from flask import Blueprint, jsonify, request, session, render_template, redirect, url_for
 from keycloak.exceptions import KeycloakAuthenticationError
+
 from keycloak_config import keycloak_openid
-import jwt
 from src.db.course_data import get_student_courses
 
 auth_bp = Blueprint('auth', __name__)
+
+
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
     username = data.get('username')
     password = data.get('password')
+    if username.endswith("@student.hu.nl"):
 
-    try:
-        token = keycloak_openid.token(username, password)
-        session['token'] = token
-        # Get courses for the student and store them in session
-        student_courses = get_student_courses(username)
-        session['student_courses'] = student_courses
+        try:
+            token = keycloak_openid.token(username, password)
+            session['token'] = token
+            # Get courses for the student and store them in session
+            student_courses = get_student_courses(username)
+            session['student_courses'] = student_courses
 
-        return jsonify({
-            'message': 'Login successful',
-            'access_token': token['access_token'],
-            'refresh_token': token.get('refresh_token'),
-            'id_token': token.get('id_token')
-        }), 200
-    except KeycloakAuthenticationError:
-        return jsonify({'message': 'Invalid username or password'}), 401
+            return jsonify({
+                'message': 'Login successful',
+                'access_token': token['access_token'],
+                'refresh_token': token.get('refresh_token'),
+                'id_token': token.get('id_token')
+            }), 200
+        except KeycloakAuthenticationError:
+            return jsonify({'message': 'Invalid username or password'}), 401
+    else:
+        try:
+            token = keycloak_openid.token(username, password)
+            session['token'] = token
+            return jsonify({
+                'message': 'Login successful',
+                'access_token': token['access_token'],
+                'refresh_token': token.get('refresh_token'),
+                'id_token': token.get('id_token')
+            }), 200
+        except KeycloakAuthenticationError:
+            return jsonify({'message': 'Invalid username or password'}), 401
 
 
 @auth_bp.route('/login')
 def login_page():
     return render_template('login/index.html')
+
 
 def get_user_roles():
     token = session.get('token', {}).get('access_token')
@@ -40,6 +58,7 @@ def get_user_roles():
         return []
     decoded_token = jwt.decode(token, options={"verify_signature": False})
     return decoded_token.get("realm_access", {}).get("roles", [])
+
 
 def login_required(f):
     @wraps(f)
@@ -54,7 +73,9 @@ def login_required(f):
             print("Invalid token: token verification failed.")
             return jsonify({'message': 'Invalid token'}), 401
         return f(*args, **kwargs)
+
     return decorated_function
+
 
 def role_required(role):
     def wrapper(f):
@@ -64,8 +85,11 @@ def role_required(role):
             if role not in roles:
                 return jsonify({'message': 'Forbidden - Access denied'}), 403
             return f(*args, **kwargs)
+
         return decorated_function
+
     return wrapper
+
 
 @auth_bp.route('/logout')
 def logout():
