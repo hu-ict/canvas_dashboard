c7eac72c60fcfb3790b6fd5eb9219c911ddb7645
Sprinkhaan <84741733+Sprinkhaan@users.noreply.github.com>
2024-12-18 13:43:21 +0100

    ENV :)

--- azure_ad_config.py
+++ azure_ad_config.py
@@ -4,6 +4,6 @@ AZURE_AD_CONFIG = {
     "CLIENT_ID": os.getenv("AZURE_SECRET_ID"),
     "CLIENT_SECRET": os.getenv("AZURE_CLIENT_SECRET_VALUE"),
     "AUTHORITY": f"https://login.microsoftonline.com/{os.getenv('AZURE_AD_TENANT_ID')}",
-    "REDIRECT_URI": "http://localhost:5101/auth/login/callback",
+    "REDIRECT_URI": f"{os.getenv('AZURE_REDIRECT_URI')}",
     "SCOPE": ["User.Read"],
 }
